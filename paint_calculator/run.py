04abdd8e3256f991d287ac0b5ede475c1a159efa
Jonny Carlyon <rr828690@fincoad.com>
2019-02-08 09:38:11 -0600

    Uses flask_bootstrap's base template, adds endpoint to do paint calculations

--- paint_calculator/run.py
+++ paint_calculator/run.py
@@ -1,7 +1,14 @@
+import json
+
 from flask import Flask, render_template, request
-import math
+from flask_bootstrap import Bootstrap
+
+from paint_calculator.api import api, sanitize_input
 
 app = Flask(__name__)
+app.register_blueprint(api)
+app.config['BOOTSTRAP_SERVE_LOCAL'] = True
+Bootstrap(app)
 
 
 @app.route('/')
@@ -29,61 +36,15 @@ def results():
     Performs most of the logic for paint calculations
     :return: Results page
     """
-    data = request.values
-    number_of_data_sets = len(data) / 3
-    all_data = []
-    total_gallons_required = 0
-
-    for room_number in range(int(number_of_data_sets)):
-        formatted_data = extract_room_info(data, room_number)
-        formatted_data['ft'] = calculate_feet(formatted_data)
-        formatted_data['gallons'] = calculate_gallons_required(formatted_data)
-        formatted_data['room'] = room_number + 1
-        total_gallons_required += calculate_gallons_required(formatted_data)
-        all_data.append(formatted_data)
-
-    return render_template("results.html", all_data=all_data, total_gallons_required=total_gallons_required)
-
-
-def calculate_feet(formatted_data):
-    """
-    Calculate the number of feet required to paint the surface area of a single room
-    :param formatted_data: dict of L/W/H information
-    :return: integer for the number of feet required by performing `((Length * 2) + (Width * 2)) * Height`
-    """
-    return int(formatted_data['length']) * int(formatted_data['width']) * int(formatted_data['height'])
-
-
-def calculate_gallons_required(formatted_data):
-    """
-    Number of feet to paint divided by the amount of feet the paint will cover, rounded up
-    :param formatted_data: An integer for the number of feet required to paint
-    :return: feet / paint coverage, rounded up
-    """
-    return math.floor(formatted_data['ft'] / 350)
-
-
-def sanitize_input(input):
-    """
-    This universe doesn't allow for negative numbers of rooms or feet
-    :param input: Any number
-    :return: The absolute, integer number
-    """
-    return abs(int(input))
-
-
-def extract_room_info(data, room_number):
-    """
-    Sanitizes inputs, and then constructs a dict of the room information
-    :param data: User input for LWH of a room
-    :param room_number: The number of the room that is being processed
-    :return: A formatted dict with room information
-    """
-    formatted_data = {'length': sanitize_input(data.get("length-%d" % room_number)),
-                      'width': sanitize_input(data.get("width-%d" % room_number)),
-                      'height': sanitize_input(data.get("height-%d" % room_number))
-                      }
-    return formatted_data
+    dimensions_data = {}
+    number_of_data_sets = int(len(request.form) / 3)
+    for i in range(number_of_data_sets):
+        dimensions_data[f'room-{i+1}'] = {
+            f'length': request.form[f'length-{i}'],
+            f'width': request.form[f'width-{i}'],
+            f'height': request.form[f'height-{i}']
+        }
+    return render_template("results.html", dimensions_data=dimensions_data, stored_data=json.dumps(dimensions_data))
 
 
 # Boiler plate for starting the application
